742342f4546271ac55b1f080986ee3c7936f8c4d
Aitor Ruano <aitor.bak@gmail.com>
2016-09-14 16:03:42 +0200

    Update rewards.cpp

--- rewards.cpp
+++ rewards.cpp
@@ -2,9 +2,8 @@
 #include "rewards.h"
 #include "nodes.h"
 #include <math.h>
-#include <algorithm>
 #include <vector>
-#include <map>
+#include <multimap>
 
 float rewardFunction(Vehicle vehicle, float desiredSpeed, float aggressivity) {
 	Vector3 position = ENTITY::GET_ENTITY_COORDS(vehicle, FALSE);
@@ -16,68 +15,6 @@ float rewardFunction(Vehicle vehicle, float desiredSpeed, float aggressivity) {
 	return aggressivity*rewardKeepSpeed(speed, desiredSpeed) + (1.0 - aggressivity)*rewardCenterOfLane(position, forwardVector);
 }
 
-//Works
-float rewardCollision(Vehicle vehicle, float currentSpeed, float max_speed) {
-	if (ENTITY::HAS_ENTITY_COLLIDED_WITH_ANYTHING(vehicle)) {
-		return -1.0;
-	}
-	return (currentSpeed / max_speed); //It is better to return reward proportional to speed or the vehicle could remain static
-}
-
-//Works but is also triggered when colliding with vehicles
-float rewardCollisionWithPed(Vehicle vehicle, float currentSpeed, float max_speed) {
-	const int numElements = 10;
-	const int arrSize = numElements * 2 + 2;
-	int nearbyPed[arrSize];
-	int count;
-	int offsettedID;
-	
-	nearbyPed[0] = numElements;
-	if (ENTITY::HAS_ENTITY_COLLIDED_WITH_ANYTHING(vehicle)) {
-		count = PED::GET_PED_NEARBY_PEDS(PLAYER::PLAYER_PED_ID(), nearbyPed, -1);
-		if (nearbyPed != NULL) {
-			for (int i = 0; i < count; i++) {
-				offsettedID = i * 2 + 2;
-				if (ENTITY::IS_ENTITY_TOUCHING_ENTITY(vehicle, nearbyPed[offsettedID])) return -1.0;
-			}
-		}
-	}
-	return (currentSpeed / max_speed);
-}
-
-
-//Works
-float rewardCollisionWithVehicle(Vehicle vehicle, float currentSpeed, float max_speed) {
-	const int arrSize = 1024;
-	Vehicle vehs[arrSize];
-	int count;
-
-	if (ENTITY::HAS_ENTITY_COLLIDED_WITH_ANYTHING(vehicle)) {
-		count = worldGetAllVehicles(vehs, arrSize);
-		for (int i = 0; i < count; i++) {
-			if (ENTITY::IS_ENTITY_TOUCHING_ENTITY(vehicle, vehs[i])) return -1.0;
-		}
-	}
-	return (currentSpeed / max_speed);
-}
-
-
-//Works
-float rewardCollisionWithObject(Vehicle vehicle, float currentSpeed, float max_speed) {
-	const int arrSize = 1024;
-	Vehicle objs[arrSize];
-	int count;
-
-	if (ENTITY::HAS_ENTITY_COLLIDED_WITH_ANYTHING(vehicle)) {
-		count = worldGetAllObjects(objs, arrSize);
-		for (int i = 0; i < count; i++) {
-			if (ENTITY::IS_ENTITY_TOUCHING_ENTITY(vehicle, objs[i])) return -1.0;
-		}
-	}
-	return (currentSpeed / max_speed);
-}
-
-//WORKS
 float rewardKeepSpeed(float currentSpeed, float desiredSpeed) {
 	float reward = currentSpeed / desiredSpeed;
 	if (reward > 1.0) reward = (1.0 - reward);
@@ -85,42 +22,8 @@ float rewardKeepSpeed(float currentSpeed, float desiredSpeed) {
 	return reward;
 }
 
-float rewardKeepSecurityDistance(Vector3 currentPosition, float min_distance){
-	const int ARR_SIZE = 1024;
-	int vehicles[ARR_SIZE];
-	std::vector<float> distances;
-	Vector3 vehicleCoords;
-	float distance;
-	float shortest_distance = min_distance;
-
-	int count = worldGetAllVehicles(vehicles, ARR_SIZE);
-	for (int i = 0; i < count; i++)
-	{
-		if (ENTITY::IS_ENTITY_ON_SCREEN(vehicles[i])) {
-			vehicleCoords = ENTITY::GET_ENTITY_COORDS(vehicles[i], FALSE);
-			distance = GAMEPLAY::GET_DISTANCE_BETWEEN_COORDS(vehicleCoords.x, vehicleCoords.y, 0, currentPosition.x, currentPosition.y, 0, 0);
-			if (distance < shortest_distance) {
-				shortest_distance = distance;
-			}
-		}
-	}
-
-	if (shortest_distance > min_distance) {
-		return 1.0;
-	}
-
-	return shortest_distance / min_distance;
-}
-
-float rewardDistanceReduction(Vector3 currentPosition, Vector3 destination, float initTotalDistance) {
-	float currentDistance = GAMEPLAY::GET_DISTANCE_BETWEEN_COORDS(currentPosition.x, currentPosition.y, 0, destination.x, destination.y, 0, 0);
-	float reward = 1 - (currentDistance / initTotalDistance);
-	if (reward < -1) reward = -1;
-	return reward;
-}
-
-//Partially works
-float rewardCenterOfLane(Vector3 currentPosition, Vector3 forwardVector) { //AND correct direction of traffic!
+//It also weights if you are in the wrong way or out of the road (negative reward)
+float rewardCenterOfLane(Vector3 currentPosition, Vector3 forwardVector) {
 
 	//Load the nodes if didn't done before
 	if (nodes.size() == 0) {
@@ -167,6 +70,3 @@ float rewardCenterOfLane(Vector3 currentPosition, Vector3 forwardVector) { //AND
 	
 	return reward;
 }
-
-
-//TODO REWARD FOR STOPPING AT STOPS AND TRAFFIC LIGHTS
