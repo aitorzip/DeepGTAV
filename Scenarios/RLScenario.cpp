87e43ea879089ba307105289d9b72904e04641d5
Aitor Ruano <aitor.bak@gmail.com>
2016-12-13 10:13:43 +0100

    Update RLScenario.cpp

--- Scenarios/RLScenario.cpp
+++ Scenarios/RLScenario.cpp
@@ -3,7 +3,7 @@
 RLScenario::RLScenario(int _car, int _initialWeather, int _weatherChangeDelay, int _initialHour, int _initialMinute, int _initialPosX, int _initialPosY, Rewarder& _rewarder) : rewarder(_rewarder), Scenario(_car, _initialWeather, _weatherChangeDelay, _initialHour, _initialMinute, _initialPosX, _initialPosY){
 }
 
-//Signal adaptation as input from control is [0,1] for throttle and [0,1] for brake. While in-game capture is [-1,1] for throttle (negative in reverse) and [0,1] for brake.
+//Signal adaptation. Iinput for control is [0,1] for throttle and [0,1] for brake. While in-game capture is [-1,1] for throttle (negative in reverse) and [0,1] for brake.
 void RLScenario::performActions(float throttle, float brake, float steering) {
 	if (brake > 0.95) { //Full-stop
 		brake = 0.0;
